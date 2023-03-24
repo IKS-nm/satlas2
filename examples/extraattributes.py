119a081bb84156ca5d4de5b7fcb5f9d9547098b8
Wouter Gins <wouter.gins@gmail.com>
2023-03-24 09:57:20 +0100

    Updated the extra attributes example

--- examples/extraattributes.py
+++ examples/extraattributes.py
@@ -15,7 +15,7 @@ class CustomLlhFitter(satlas2.Fitter):
         try:
             bunches = self.bunches
         except:
-            bunches = f.getSourceAttr('bunches')
+            bunches = self.getSourceAttr('bunches')
             self.bunches = bunches
         try:
             data_counts = self.data_counts
@@ -37,43 +37,44 @@ def modifiedSqrt(y, bunches=None):
     yerr[y <= 0] = 1/bunches[y <= 0]
     return yerr
 
-spin = 0
+spin = 1
 J = [0, 1]
-A = [0, 0]
+A = [0, 50]
 B = [0, 0]
 C = [0, 0]
 FWHMG = 135/4
 FWHML = 101/25
 centroid = 0
 bkg = 0.0005
-scale = 0.003
+scale = 0.001
 
 hfs = satlas2.HFS(spin, J, A, B, C, df=centroid, fwhmg=FWHMG, fwhml=FWHML, scale=scale)
 background = satlas2.Polynomial([bkg])
 
-bunches_background = 500
-bunches_amplitude = 100000
+bunches_background = 100000
+noise = min(10, bunches_background/5) 
+# bunches_amplitude = 100000
 
-sampling_peak = satlas2.Voigt(bunches_amplitude, centroid, FWHMG*0.5, FWHML*0.5)
+# sampling_peak = satlas2.Voigt(bunches_amplitude, centroid, FWHMG*0.5, FWHML*0.5)
 sampling_background = satlas2.Polynomial([bunches_background])
 
 models = [hfs, background]
 
 x = np.arange(-150, 150, 5)
 y = []
-bunches = []
 rng = np.random.default_rng(10)
-for X in x:
-    evaluated = int(sampling_background.f(X) + sampling_peak.f(X))
+bunches = satlas2.generateSpectrum(sampling_background, x, lambda x: rng.normal(x, noise))
+bunches = np.ones(bunches.shape)*np.max(bunches)
+bunches = bunches.astype(int)
+for X, evaluated in zip(x, bunches):
     Y = satlas2.generateSpectrum(models, np.array([X]*evaluated), rng.poisson)
     y.append(Y.sum()/evaluated)
-    bunches.append(evaluated)
 y = np.array(y)
-bunches = np.array(bunches)
 
 yerrCalc = functools.partial(modifiedSqrt, bunches=bunches)
 
-source = satlas2.Source(x, y, yerr=yerrCalc, name='Data', bunches=bunches)
+# source = satlas2.Source(x, y, yerr=yerrCalc, name='Data', bunches=bunches)
+source = satlas2.Source(x, y, yerr=modifiedSqrt(y, bunches), name='Data', bunches=bunches)
 f = CustomLlhFitter()
 f.addSource(source)
 source.addModel(hfs)
@@ -95,6 +96,8 @@ ax_rate = fig.add_subplot(gs[2, :])
 ax_events.plot(source.x, source.bunches, drawstyle='steps-mid')
 ax_counts.plot(x, y*bunches, drawstyle='steps-mid', label='Data')
 ax_rate.errorbar(x, y, yerr=source.yerr(), drawstyle='steps-mid', label='Data')
+plot_y = hfs.f(plot_x)+background.f(plot_x)
+ax_rate.plot(plot_x, plot_y, label='Initial')
 f.fit()
 plot_y = hfs.f(plot_x)+background.f(plot_x)
 ax_rate.plot(plot_x, plot_y, label='Gaussian fit')
@@ -112,5 +115,8 @@ ax_rate.label_outer()
 ax_rate.set_ylabel('Rate [cts/bunch]')
 ax_events.set_ylabel('Number of bunches [-]')
 ax_counts.set_ylabel('Raw counts')
+
+data = np.vstack([x, y*bunches]).T
+np.savetxt('testdata.txt', data, delimiter=',')
 
 plt.show()
